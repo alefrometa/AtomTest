d6b985266dd2871be8015cedf33e17da67435a29
Alejandro Frometa <alejandro.frometa@gmail.com>
2017-01-22 09:48:32 -0500

    First commit testing out the git Plus extension on Atom

---
+++ jsxTest.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Button = React.createClass({
+  scream: function () {
+    alert('AAAAAAAAHHH!!!!!');
+  },
+
+  render: function () {
+    return <button onClick={this.scream}>AAAAAH I kill you!</button>;
+  }
+});
