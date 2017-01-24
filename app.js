0a4d34affd4325689a1f96430234e133e8f33f77
Alejandro Frometa <alejandro.frometa@gmail.com>
2017-01-23 19:56:08 -0500

    Working node app with bootstrap

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var pug = require('pug');
+var routes = require('./routes/routes');
+//var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/public/views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
@@ -23,8 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-
+//app.use('/users', users);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
