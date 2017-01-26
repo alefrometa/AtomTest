e6f07f9f740745d3584dcec9a64b25a71f87f413
Alejandro Frometa <alejandro.frometa@gmail.com>
2017-01-25 20:58:33 -0500

    More concise JSX code for React.

--- asset/jsx/reactApp.jsx
+++ asset/jsx/reactApp.jsx
@@ -1,45 +1,19 @@
-console.log("Hello world!");
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-class Layout extends React.Component{
-  render () {
-    return (
-      <div className="container">
-        <h1>It Works! Ehehehehehe?</h1>
-        <p>And this is how you template out a React app</p>
-        <p className="mom">Yo mama joke here</p>
-      </div>
-    );
-  }
-}
-
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
-
-
-/*
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-//Routing
-var Router = require('react-router').Router;
-var Route = require('react-router').Route;
-var Link = require('react-router').Link;
-
-let Button = React.createClass({
+var Button = React.createClass({
   scream: function () {
-    alert('AAAAAAAAHHH!!!!');
+    alert('AAAAAAAAHHH!!!!!');
   },
+
   render: function () {
-    return (
-      <div className="container">
-      <button type="button" className="btn btn-default" onClick={this.scream}><a href='#'>Scream!</a></button>
-      </div>
+    return (<div className="container">
+      <h1>Hello There!</h1>
+      <h3>This is justa jest...uh test..jest test.</h3>
+    </div>
     );
   }
 });
 
-ReactDOM.render(<Button />, document.getElementById('app') );
-*/
+var app = document.getElementById('app');
+ReactDOM.render(<Button />, app);
